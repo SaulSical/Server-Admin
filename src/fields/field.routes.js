d8ec4ab1002db441264330a7bc62fa9f1fdaad63
Saul Sical <ssical-2024422@kinal.edu.gt>
2026-02-11 17:03:54 -0600

    Preoyecto casi terminado

--- src/fields/field.routes.js
+++ src/fields/field.routes.js
@@ -1,16 +1,23 @@
 import { Router} from 'express';
 import { createField, getFields } from "./field.controller.js";
-
+import { uploadFileImage } from '../../middlewares/file-uploder.js';
+import { cleanUploaderFileOnFinish } from '../../middlewares/delete-file-on-error.js';
+import { validate } from 'uuid';
+import { validateCreateField } from '../../middlewares/validateCreateField.js';
+ 
 const router = Router();
-
+ 
 router.post(
     '/create',
+    uploadFileImage.single('image'),
+    cleanUploaderFileOnFinish,
+    validateCreateField,
     createField
 )
-
+ 
 router.get(
     "/get",
     getFields
 )
-
+ 
 export default router;
